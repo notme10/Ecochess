f950832dcb00c2c5a14c206fbdb3687602dfaaac
notme10 <80652430+notme10@users.noreply.github.com>
2022-04-10 15:54:14 -0700

    coordinate stuff

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // MaYBE TRY RESTARTING THE SERVERY :)
 
 /* The express module is used to look at the address of the request and send it to the correct function */
-var express = require('express');
-var bodyParser = require('body-parser');
-var http = require('http'); /* The http module is used to listen for requests from a web browser */
-var path = require('path'); /* The path module is used to transform relative paths to absolute paths */
+var express = require("express");
+var bodyParser = require("body-parser");
+var http = require("http"); /* The http module is used to listen for requests from a web browser */
+var path = require("path"); /* The path module is used to transform relative paths to absolute paths */
 // var mongoose = require('mongoose');
-var Io = require('socket.io');
+var Io = require("socket.io");
 // var dbAddress = process.env.MONGODB_URI || 'mongodb://127.0.0.1/spacecrash';
 
 /* Creates an express application */
@@ -18,138 +18,205 @@ var server = http.createServer(app);
 var io = Io(server);
 var axios = require("axios").default;
 
-
 /* Defines what port to use to listen to web requests */
-var port =  process.env.PORT
-? parseInt(process.env.PORT):
-8080;
+var port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
 function addSockets() {
+    var rooms = {};
 
-	var rooms = {};
+    io.on("connection", (socket) => {
+        // server listens for a connection from a client
 
-	io.on('connection', (socket) => { // server listens for a connection from a client
+        var name; // server-side variables for specific connections
+        var roomId;
+        socket.on("setRoom", (data) => {
+            roomId = data.room;
+            if (!rooms[roomId]) {
+                rooms[roomId] = {
+                    w: null,
+                    b: null,
+                    pieces: null,
+                    turn: 0,
+                    moveList: [],
+                };
+            }
+        });
+        socket.on("playerName", (data) => {
+            name = data.name;
 
-		var name; // server-side variables for specific connections
-		var roomId;
-		socket.on('setRoom', (data) => {
-			roomId = data.room;
-			if (!rooms[roomId]) {
-				rooms[roomId] = {"w": null, "b": null, "pieces": null, "turn": 0, "moveList": []};
-			}
-		})
-		socket.on('playerName', (data) => {
-			name = data.name;
+            if (rooms[roomId]["w"] == null) {
+                rooms[roomId]["w"] = name;
+            } else if (rooms[roomId]["b"] == null) {
+                rooms[roomId]["b"] = name;
+            }
 
-			if(rooms[roomId]["w"] == null) {
-				rooms[roomId]["w"] = name;
-			} else if(rooms[roomId]["b"] == null) {
-				rooms[roomId]["b"] = name;
-			}
+            io.emit("sidesInfo", rooms[roomId]);
+            io.emit("playerConnect", {
+                name: name,
+                info: rooms[roomId],
+            }); // emits a message
+        });
 
-			io.emit('sidesInfo', rooms[roomId])
-			io.emit('playerConnect', {
-				name: name,
-				info: rooms[roomId]
-			}); // emits a message
-		})
+        socket.on("makeMove", (data) => {
+            rooms[roomId]["pieces"] = data.pieces;
+            rooms[roomId]["turn"] = data.turn;
+            var movesListArray = rooms[roomId]["moveList"];
+            var lastPiece = movesListArray[movesListArray.length - 1];
+            if (!lastPiece || lastPiece.pieceName !== data.moves.pieceName) {
+                rooms[roomId]["moveList"].push(data.moves);
+            }
+            io.emit("sendMove", data);
+        });
 
-		socket.on('makeMove', (data) => {
-			rooms[roomId]["pieces"] = data.pieces;
-			rooms[roomId]["turn"] = data.turn;
-			var movesListArray = rooms[roomId]["moveList"];
-			var lastPiece = movesListArray[movesListArray.length - 1];
-			if(!lastPiece || lastPiece.pieceName !== data.moves.pieceName) {
-				rooms[roomId]["moveList"].push(data.moves);
-			}
-			io.emit("sendMove", (data));
-		});
+        socket.on("disconnect", (data) => {
+            // server listens for a disconnection
 
-		socket.on('disconnect', (data) => { // server listens for a disconnection
+            // Do something
 
-			// Do something
+            if (rooms[roomId]) {
+                if (rooms[roomId]["w"] == name) {
+                    io.emit("playerDisconnect", "w");
+                } else if (rooms[roomId]["b"] == name) {
+                    io.emit("playerDisconnect", "b");
+                }
+            }
 
-			if(rooms[roomId]) {
-				if(rooms[roomId]["w"] == name) {
-					io.emit('playerDisconnect', "w");
-				} else if(rooms[roomId]["b"] == name) {
-					io.emit('playerDisconnect', "b");
-				}
-			}
-
-			if (rooms[roomId] == null || rooms[roomId]["w"] == null && rooms[roomId]["b"] == null) {
-					rooms[roomId] = null;
-			}
-			else {
-				// if (rooms[roomId]["w"] == name) {
-				rooms[roomId]["w"] = null;
-				// }
-				// if (rooms[roomId]["b"] == name) {
-				rooms[roomId]["b"] = null;
-				// }
-			}
-			io.emit('sidesInfo', rooms[roomId])
-		});
-
-	});
-
+            if (
+                rooms[roomId] == null ||
+                (rooms[roomId]["w"] == null && rooms[roomId]["b"] == null)
+            ) {
+                rooms[roomId] = null;
+            } else {
+                // if (rooms[roomId]["w"] == name) {
+                rooms[roomId]["w"] = null;
+                // }
+                // if (rooms[roomId]["b"] == name) {
+                rooms[roomId]["b"] = null;
+                // }
+            }
+            io.emit("sidesInfo", rooms[roomId]);
+        });
+    });
 }
 
 function startServer() {
+    addSockets();
 
-	addSockets();
+    app.use(bodyParser.json({ limit: "16mb" }));
+    app.use(express.static(path.join(__dirname, "public")));
 
-	app.use(bodyParser.json({ limit: '16mb' }));
-	app.use(express.static(path.join(__dirname, 'public')));
+    app.get("/", (req, res, next) => {
+        var filePath = path.join(__dirname, "./game.html");
+        res.sendFile(filePath);
+    });
 
-	app.get('/', (req, res, next) => {
-		var filePath = path.join(__dirname, './game.html');
-		res.sendFile(filePath);
-	})
+    app.post("/", (req, res, next) => {
+        console.log(req.body);
+        res.send("OK");
+    });
+    app.get("/ipTest", (req, res, next) => {
+        var options = {
+            method: "GET",
+            url: "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/",
+            headers: {
+                "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+                "x-rapidapi-key":
+                    "72e9555b18msh88c262cebd13cc6p1082bejsnb6b410214595",
+            },
+        };
 
-	app.post('/', (req, res, next) => {
-		console.log(req.body);
-		res.send('OK');
-	})
-	app.get('/ipTest', (req, res, next) => {
-		var options = {
-		  method: 'GET',
-		  url: 'https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/',
-		  headers: {
-		    'x-rapidapi-host': 'ip-geolocation-ipwhois-io.p.rapidapi.com',
-		    'x-rapidapi-key': '72e9555b18msh88c262cebd13cc6p1082bejsnb6b410214595'
-		  }
-		};
+        axios
+            .request(options)
+            .then(function (response) {
 
-		axios.request(options).then(function (response) {
-			console.log(response.data);
-			let city = `${response.data.city}, ${response.data.region}`
-			res.send(city);
-		}).catch(function (error) {
-			console.error(error);
-			res.send(error);
-		});
-		// console.dir(req.ip)
-		// res.send(req.ip);
-	})
+                var curX = response.data.latitude;
+                var curY = response.data.longitude;
 
-	/* Defines what function to all when the server recieves any request from http://localhost:8080 */
-	server.on('listening', () => {
+                let citiesObject = {
+                    SanFransisco: {
+                        x: 122,
+                        y: 38,
+                        biome: "Ocean",
+                    },
+                    Veracruz: {
+                        x: 96,
+                        y: 19,
+                        biome: "Forest",
+                    },
+                    Phoenix: {
+                        x: 112,
+                        y: 33,
+                        biome: "Desert",
+                    },
+                    Winnipeg: {
+                        x: 97,
+                        y: 50,
+                        biome: "Tundra",
+                    },
+                    Miami: {
+                        x: 80,
+                        y: 26,
+                        biome: "Reef",
+                    },
+                };
 
-		/* Determining what the server is listening for */
-		var addr = server.address()
-		, bind = typeof addr === 'string'
-		? 'pipe ' + addr
-		: 'port ' + addr.port
-		;
+                function distCalc(curX, curY, targetX, targetY) {
+                    return Math.abs(
+                        Math.sqrt(
+                            (curX - targetX) * (curX - targetX) +
+                                (curY - targetY) * (curX - targetX)
+                        )
+                    );
+                }
 
-		/* Outputs to the console that the webserver is ready to start listenting to requests */
-		console.log('Listening on ' + bind);
-	});
+                var nearestCity;
+                var shortestDistance = Number.MAX_SAFE_INTEGER;
+                var cityBiome;
 
-	/* Tells the server to start listening to requests from defined port */
-	server.listen(port);
+                for (var cityKey in citiesObject) {
+                    var targetX = citiesObject[cityKey].x;
+                    var targetY = citiesObject[cityKey].y;
+                    var distance = distCalc(curX, curY, targetX, targetY);
 
+                    if (distance < shortestDistance) {
+                        shortestDistance = distance;
+                        nearestCity = cityKey;
+                        cityBiome = citiesObject[cityKey].biome;
+                    }
+                }
+
+                console.log(
+                    "The nearest city is " +
+                        nearestCity +
+                        ". Its biome is " +
+                        cityBiome +
+                        "."
+                );
+
+                console.log(response.data);
+                let city = `${response.data.city}, ${response.data.region}`;
+                res.send(city);
+            })
+            .catch(function (error) {
+                console.error(error);
+                res.send(error);
+            });
+        // console.dir(req.ip)
+        // res.send(req.ip);
+    });
+
+    /* Defines what function to all when the server recieves any request from http://localhost:8080 */
+    server.on("listening", () => {
+        /* Determining what the server is listening for */
+        var addr = server.address(),
+            bind =
+                typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+        /* Outputs to the console that the webserver is ready to start listenting to requests */
+        console.log("Listening on " + bind);
+    });
+
+    /* Tells the server to start listening to requests from defined port */
+    server.listen(port);
 }
 
 // mongoose.connect(dbAddress, startServer);
--- public/coords.js
+++ public/coords.js
@@ -1,73 +1,141 @@
-var curX = 122;
-var curY = 40;
+var curX = 96;
+var curY = 17;
 
-// replace this with an object instead of an array {}
-// figure out how to iterate through an object
-// replace the XY coors with actual longitude and latitude coords
-var cities = [
-    sf = {
-        City: "San Fransisco",
-        X_coord: "122",
-        Y_coord: "38",
-        Biome: "Ocean"
+let citiesObject = {
+
+    "San Fransisco": {
+        x: 122,
+        y: 38,
+        biome: "Ocean"
+    }, 
+    "Veracruz": {
+        x: 96,
+        y: 19,
+        biome: "Forest"
+    }, 
+    "Phoenix": {
+        x: 112,
+        y: 33,
+        biome: "Desert"
     },
-
-    {
-        City: "Veracruz",
-        X_coord: "96",
-        Y_coord: "19",
-        Biome: "Forest"
+    "Winnipeg": {
+        x: 97,
+        y: 50,
+        biome: "Tundra"
     },
-
-    {
-        City: "Phoenix",
-        X_coord: "112",
-        Y_coord: "33",
-        Biome: "Desert"
-    },
-
-    {
-        City: "Winnipeg",
-        X_coord: "97",
-        Y_coord: "50",
-        Biome: "Tundra"
-    },
-
-    {
-        City: "Miami",
-        X_coord: "80",
-        Y_coord: "26",
-        Biome: "Reef"
+    "Miami": {
+        x: 80,
+        y: 26,
+        biome: "Reef"
     }
-];
 
-// this func name is unnecessarily long
-function distanceCalculator(curX, curY, targetX, targetY) {
+}
+if (navigator.geolocation) { //check if geolocation is available
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log(position.latitude);
+    });
+}
+
+function distCalc(curX, curY, targetX, targetY) {
     return Math.abs(Math.sqrt((curX - targetX) * (curX - targetX) + (curY - targetY) * (curX - targetX)));
 }
 
-var nearestCity = "";
-var shortestDistance = 100000000000;
-// what if the shortest distance is larger than that number? What's the LARGEST number you can get in JS?
+var nearestCity;
+var shortestDistance = Number.MAX_SAFE_INTEGER;
+var cityBiome;
 
-for (var i = 0; i < cities.length - 1; i++) {
-    var targetX = cities[i]["X_coord"];
-    var targetY = cities[i]["Y_coord"];
-    var distance = distanceCalculator(curX, curY, targetX, targetY);
+for(var city in citiesObject) {
+    var targetX = citiesObject[city].x;
+    var targetY = citiesObject[city].y;
+    var distance = distCalc(curX, curY, targetX, targetY);
 
     if (distance < shortestDistance) {
         shortestDistance = distance;
-        nearestCity = cities[i]["City"];
+        nearestCity = city;
+        cityBiome = citiesObject[city].biome;
     }
 }
 
-console.log(nearestCity);
+console.log("The nearest city is " + nearestCity + ". Its biome is " + cityBiome + ".");
 
-// get the distance from current location to each important coordinate
-// create a variable to store the shortest distance
-// after going through every distance, assign the user to that biome
 
+
+
+// DONE replace this with an object instead of an array {}
+// DONE figure out how to iterate through an object
+// replace the XY coors with actual longitude and latitude coords
+
+// DONE what if the shortest distance is larger than that number? What's the LARGEST number you can get in JS?
+
+// DONE get the distance from current location to each important coordinate
+// DONE create a variable to store the shortest distance
+// DONE after going through every distance, assign the user to that biome
 
 // AFTER you've finished the comments that I made:
 // Figure out how to use the coords given by the /ipTest thing in index.js
 // and figure out the city that is closest to your coordinates
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------OLD-CODE---------------------
+
+
+// var cities = [
+//     {
+//         City: "San Fransisco",
+//         X_coord: "122",
+//         Y_coord: "38",
+//         Biome: "Ocean"
+//     },
+
+//     {
+//         City: "Veracruz",
+//         X_coord: "96",
+//         Y_coord: "19",
+//         Biome: "Forest"
+//     },
+
+//     {
+//         City: "Phoenix",
+//         X_coord: "112",
+//         Y_coord: "33",
+//         Biome: "Desert"
+//     },
+
+//     {
+//         City: "Winnipeg",
+//         X_coord: "97",
+//         Y_coord: "50",
+//         Biome: "Tundra"
+//     },
+
+//     {
+//         City: "Miami",
+//         X_coord: "80",
+//         Y_coord: "26",
+//         Biome: "Reef"
+//     }
+// ];
+
+
+
+// for (var i = 0; i < cities.length - 1; i++) {
+//     var targetX = cities[i]["X_coord"];
+//     var targetY = cities[i]["Y_coord"];
+//     var distance = distanceCalculator(curX, curY, targetX, targetY);
+
+//     if (distance < shortestDistance) {
+//         shortestDistance = distance;
+//         nearestCity = cities[i]["City"];
+//     }
+// }
