74852f13c139948017c5ead72b1befc090446a35
notme10 <80652430+notme10@users.noreply.github.com>
2022-09-25 14:46:06 -0700

    added an end game function

--- public/sockets.js
+++ public/sockets.js
@@ -2,15 +2,9 @@
  * @desc listens for playerConnect, logs player's name and information
  */
 socket.on("playerConnect", (data) => {
-    console.log(data.name);
-    console.log(data.info);
-
-    // if (side == 'w') {
-    //     writeMessageBoard("white has connected");
-    // }
-    // else {
-    //     writeMessageBoard("black has connected")
-    // }
+    if(data.info.b) {
+        gameInProgress = true;
+    }
 });
 
 /**
@@ -21,20 +15,6 @@ socket.on("sendMove", (data) => {
         let pieceOnCoord = game.board.configuration.pieces[data.toCoords];
         let eatenPiece = pieceOnCoord ? pieceOnCoord : ""
         makeMove(data.fromCoords, data.toCoords, data.side, eatenPiece);
-
-        {
-        // cellCoord = data.moves.newPos.substring(0, 2);
-        // pieceInfo = [data.moves.pieceName, data.moves.oldPos];
-        // enemyPromotion = data.moves.display.split("=")[1];
-        // console.log(data);
-        // if (virtualBoard[convertRowsToIndex(data.moves.oldPos[1])][convertColsToIndex(data.moves.oldPos[0])]) {
-        //     if(virtualBoard[convertRowsToIndex(cellCoord[1])][convertColsToIndex(cellCoord[0])]) {
-        //         virtualBoard[convertRowsToIndex(data.moves.oldPos[1])][convertColsToIndex(data.moves.oldPos[0])].eat();
-        //     } else {
-        //         virtualBoard[convertRowsToIndex(data.moves.oldPos[1])][convertColsToIndex(data.moves.oldPos[0])].move();
-        //     }
-        // }
-        }
     }
 });
 
@@ -42,35 +22,16 @@ socket.on("sendMove", (data) => {
  * @desc listens for sidesInfo, initializes side variable
  */
 socket.on("sidesInfo", (data) => {
-    // data is null pls fix
     console.log(data);
     if (!data) {
     } else if(data.w == name && side !== "w") {
         side = "w";
-        // board.innerHTML = '';
-        // generateBoard(side);
     } else if(data.b == name && side !== "b") {
         side = "b";
-        // board.innerHTML = '';
         flipBoard();
-        // generateBoard(side);
     } else if(side !== "w" && side !== "b") {
         side = "s";
-        // board.innerHTML = '';
-        // generateBoard(side);
     }
-    // if there are two boards, fix this code
 })
-
-/**
- * @desc listens for playerDisconnect, show text on opponent's screen that opponent has disconnected
- */
-// socket.on("playerDisconnect", (data) => {
-//     if(data == "w") {
-//         writeMessageBoard("white has disconnected");
-//     } else {
-//         writeMessageBoard("black has disconnected");
-//     }
-// });
 
 
